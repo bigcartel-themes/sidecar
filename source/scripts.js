b8aa9dc8d29fa063cefd1475e374c424fb7f507e
Blake Walters <blake@markupboy.com>
2013-03-14 13:43:14 -0600

    moved vendor javascripts to js root

--- source/scripts.js
+++ source/scripts.js
@@ -1,2 +1,4 @@
-//= require_directory ./javascripts/vendor
+//= require javascripts/jquery.fancybox.pack
+//= require javascripts/jquery.infinitescroll.min
+//= require javascripts/jquery.isotope.min
 //= require javascripts/store
