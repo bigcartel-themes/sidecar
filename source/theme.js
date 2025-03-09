bc282352b8b39dbc2b203db6faa3ad4899d01d98
Trevin Chow <trevin@trevinchow.com>
2025-03-09 15:48:39 -0700

    feat: slideshow capability for homepage

--- source/theme.js
+++ source/theme.js
@@ -1,8 +1,8 @@
 //= require_directory ./javascripts/vendor
-
 //= require javascripts/store
 //= require javascripts/product
 //= require javascripts/cart
 //= require javascripts/product-option-groups
 //= require javascripts/product-carousel
 //= require javascripts/search
+//= require javascripts/home-carousel
